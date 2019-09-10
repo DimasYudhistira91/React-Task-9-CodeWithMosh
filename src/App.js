d72d4a14776e082b50cf3c70ca4687d93b2fc295
DimasYudhistira91 <dimasmillaty@gmail.com>
2019-09-10 14:49:11 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -15,14 +15,20 @@ class App extends Component {
       <div>
         <NavBar />
         <div className='content'>
-          <Route path='/products' component={Products} />
-          <Route path='/posts' component={Posts} />
-          <Route path='/admin' component={Dashboard} />
-          <Route path='/' component={Home} />
+          <Switch>
+            <Route path='/products/:id' component={ProductDetails} />
+            <Route path='/products' render={(props) => <Products sortBy='newest' {...props} />} />
+            <Route path='/posts/:year?/:month?' component={Posts} />
+            <Route path='/admin' component={Dashboard} />
+            <Redirect from='/messages' to='/posts' />
+            <Route path='/not-found' component={NotFound} />
+            <Route path='/' exact component={Home} />
+            <Redirect to='/not-found' />
+          </Switch>
         </div>
       </div>
     );
   }
 }
 
-export default App;
+export default App; 
